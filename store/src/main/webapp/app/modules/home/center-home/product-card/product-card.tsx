0b0a52191275f4279e2fd78afbe36a499d942284
Andres Peralta <aperalta@technisys.com>
2023-10-25 07:59:54 -0500

    added product modal detail

--- store/src/main/webapp/app/modules/home/center-home/product-card/product-card.tsx
+++ store/src/main/webapp/app/modules/home/center-home/product-card/product-card.tsx
@@ -6,31 +6,38 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ProductModal from '../product-modal/product-modal';
 
 export const ProductCard = props => {
   const { product } = props;
+
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
-    <Col sm={4}>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-        <Card.Body>
-          <Card.Title>{product.name}</Card.Title>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>{product.price}</ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Row>
-            <Col>
-              <Card.Link href="#">Ver detalles</Card.Link>
-            </Col>
-            <Col>
-              <Button variant="primary">Agregar</Button>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    </Col>
+    <>
+      <ProductModal show={modalShow} onHide={() => setModalShow(false)} product={product} />
+      <Col sm={4}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>{product.price}</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Row>
+              <Col>
+                <Card.Link onClick={() => setModalShow(true)}>Ver detalles</Card.Link>
+              </Col>
+              <Col>
+                <Button variant="primary">Agregar</Button>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
   );
 };
 
---
+++ store/src/main/webapp/app/modules/home/center-home/product-modal/product-modal.tsx
@@ -0,0 +1,47 @@
+import './product-modal.scss';
+import React from 'react';
+import { connect } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+
+export const ProductModal = props => {
+  const { product } = props;
+  return (
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Container>
+          <Row>
+            <Col md={5}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+            </Col>
+            <Col md={7}>
+              <Row>
+                <h4>{product.name}</h4>
+              </Row>
+              <Row>
+                <p>{product.description}</p>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.onHide}>
+          Cerrar
+        </Button>
+        <Button variant="primary" onClick={props.onHide}>
+          Agregar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default connect()(ProductModal);
